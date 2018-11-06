e7bca9b1e890fc9f489d99448ee0a39eb6b1d411
Nils Baumgartner <nilsbaumgartner1994@gmail.com>
2018-11-06 11:02:50 +0100

    Rendering Tiles of a Chunk (wrong)

---
+++ Classes/entitiys/Human.cpp
@@ -0,0 +1,8 @@
+//
+//  Human.cpp
+//  PixelWorld
+//
+//  Created by Nils Baumgartner on 06.11.18.
+//
+
+#include "Human.hpp"
---
+++ Classes/entitiys/Human.hpp
@@ -0,0 +1,13 @@
+//
+//  Human.hpp
+//  PixelWorld
+//
+//  Created by Nils Baumgartner on 06.11.18.
+//
+
+#ifndef Human_hpp
+#define Human_hpp
+
+#include <stdio.h>
+
+#endif /* Human_hpp */
